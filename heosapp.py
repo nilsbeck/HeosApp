f91c4187470c54926d4957b496b6aa7ebe53cac5
Nils Beckmann <>
2021-10-30 20:27:43 +0200

    fixes bugs, cleans code

--- heosapp.py
+++ heosapp.py
@@ -30,6 +30,7 @@ async def setup(heos: Pytheos):
 
 
 async def updateQueue():
+    await asyncio.sleep(0.5)
     queue = await heos.api.player.get_queue(player.id)
     #print(queue)
     cleanedQueue = [[item.song, item.album, item.artist] for item in queue]
@@ -82,11 +83,14 @@ async def playPrevious():
 
 async def playPause():
     if heos.connected == True:
-        state = await heos.api.player.get_play_state(player.id)
-        if state.value == 'stop' or state.value == 'pause':
-            await heos.api.player.set_play_state(player.id, PlayState('play'))
-        else:
-            await heos.api.player.set_play_state(player.id, PlayState('stop'))
+        try:
+            state = await heos.api.player.get_play_state(player.id)
+            if state.value == 'stop' or state.value == 'pause':
+                await heos.api.player.set_play_state(player.id, PlayState('play'))
+            else:
+                await heos.api.player.set_play_state(player.id, PlayState('pause'))
+        finally:
+            print('error occurred.')
 
 
 async def setVolume(volume: int):
@@ -107,7 +111,7 @@ async def addToQueue(d: dict):
         # PlayNext = 2
         # AddToEnd = 3
         # ReplaceAndPlay = 4
-        await heos.api.browse.add_to_queue(player.id, '5', media.container_id, media.media_id, add_type=AddToQueueType.AddToEnd)
+        await heos.api.browse.add_to_queue(player.id, '5', media.container_id, media.media_id, add_type=3)
         await updateQueue()
 
 async def search(searchString: str):
@@ -117,27 +121,27 @@ async def search(searchString: str):
         # search_criteria: 2 - album
         # search_criteria: 3 - track
         searchCriteria = 1
-        if searchString[0:2] == '/1':
+        if searchString[0:1] == ('1' or 'b'):
             print('search: artist')
-        elif searchString[0:2] == '/2':
+        elif searchString[0:1] == ('2' or 'a'):
             print('search: album')
             searchCriteria = 2
-        elif searchString[0:2] == '/3':
+        elif searchString[0:1] == ('3' or 't'):
             print('search: track')
             searchCriteria = 3
         else:
             return
-        searchString = searchString[2:]
+        searchString = searchString[1:].strip()
         tracks = await heos.api.browse.search(5, searchString, searchCriteria)
-        values = [[source.name] for source in tracks]
+        values = [[source.name, source.artist, source.album] for source in tracks]
         window['-SRESULT-'].update(values)
         window['-SRESULT-'].metadata = tracks
 
 col1 = [
-    [sg.In(size=(30, 1), justification=LEFT, enable_events=TRUE, key='-SEARCH-', default_text='/1meatallica')],
-    [sg.Table(values=[['', '', '']], headings=['Name'], key='-SRESULT-',
+    [sg.In(size=(30, 1), justification=LEFT, enable_events=TRUE, key='-SEARCH-')],
+    [sg.Table(values=[['', '', '']], headings=['Name', 'Artist', 'Album'], key='-SRESULT-',
               justification=LEFT, size=(90, 50), def_col_width=25,
-              bind_return_key=True,
+              enable_click_events=True, bind_return_key=True,
               auto_size_columns=False, display_row_numbers=True)]
 ]
 
@@ -148,8 +152,8 @@ col2 = [
         sg.Button(button_text='Next', key='-NEXT-')
     ],
     [sg.Table(values=[['', '', '']], headings=['Song', 'Album', 'Artist'], key='-QUEUE-',
-              justification=LEFT, size=(90, 50), col_widths=[30, 20, 20],
-              #bind_return_key=True,
+              justification=LEFT, size=(90, 50), col_widths=[30, 20, 20], enable_click_events=True,
+              bind_return_key=True,
               auto_size_columns=False, display_row_numbers=True)]
 ]
 
@@ -159,7 +163,22 @@ layout = [
 
 # Create the window
 window = sg.Window("HEOS Player", layout, finalize=True, return_keyboard_events=True)
-loop = True
+# used for start/stop
+window.bind("<Command-s>", "Control + s")
+window.bind("<Control-s>", "Control + s")
+# search -> search + future command palette
+window.bind("<Command-p>", "Control + p")
+window.bind("<Control-p>", "Control + p")
+# next song
+# window.bind("<Command-right>", "Control + right")
+# window.bind("<Control-right>", "Control + right")
+# # prev song
+# window.bind("<Command-left>", "Control + left")
+# window.bind("<Control-left>", "Control + left")
+# options for adding to queue
+window.bind("<Command-Return>", "Control + return")
+window.bind("<Control-Return>", "Control + return")
+
 heos = Pytheos
 player = Player
 
@@ -172,26 +191,24 @@ async def main():
 
     print("Connecting to first device found...")
     global heos
-    heos = await pytheos.connect(services[0])  # '192.168.178.25'
-    await heos.api.heart_beat()
-    await heos.sign_in(username='nils.beckmann@gmail.com', password='7Rqok3qk8GZDQ4jB')
+    heos = await pytheos.connect(services[0])
     print(f"Connected to {heos.server}!")
     players = await heos.get_players()
     global player
     player = players[0]
-    #window['-TEXT-'].update(f"Status: Connected to {player.name}")
+    window.set_title(f'HEOS App ({player.name})')
     heos.subscribe('event/player_state_changed', _on_player_state_changed)
     heos.subscribe('event/player_now_playing_changed',
                    _on_now_playing_changed)
     heos.subscribe('event/player_queue_changed', _on_queue_changed)
-    window['-SEARCH-'].update(value='/1', move_cursor_to="end")
+    window['-SEARCH-'].update(value='1 ', move_cursor_to="end")
     await updateQueue()
     test = await heos.get_sources()
     print(test)
     while True:
         # timeout in window.read() are needed to not make the event
         # listener hang up himself
-        event, values = window.read(timeout=100)
+        event, values = window.read(timeout=10)
         if event != '__TIMEOUT__':
             print(f"event: {event}")
             print(f"values: {values}")
@@ -199,13 +216,23 @@ async def main():
             # End program if user closes window
             if event == sg.WIN_CLOSED:
                 break
-            
-            elif event == '-PLAY-':
+            # used window.bind previously to create key combos
+            elif event == 'Control + p':
+                window['-SEARCH-'].set_focus()
+            elif event == 'Control + return':
+                print('feature not yet implemented')
+                # TODO: add options function
+            elif event == '-PLAY-'or event == 'Control + s':
                 await playPause()
-            elif event == '-PREV-':
+            elif event == '-PREV-' or event == 'Control + left':
                 await playPrevious()
-            elif event == '-NEXT-':
+            elif event == '-NEXT-' or event == 'Control + right':
                 await playNext()
+            elif event == '-QUEUE-':
+                print('play a song')
+                await playFromQueue(values)
+            elif event == '-SRESULT-':
+                await addToQueue(values)
             # React if return key was pressed
             elif event == 'Return:603979789':
                 if elem is not None:
@@ -220,11 +247,6 @@ async def main():
                     # If search results are selected, add them to queue
                     elif elem.Key == '-SRESULT-':
                         await addToQueue(values)
-            elif event == '-QUEUE-':
-                print('play a song')
-                await playFromQueue(values)
-            elif event == '-SRESULT-':
-                await addToQueue(values)
 
     window.close()
 
